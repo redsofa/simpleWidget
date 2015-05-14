d4881e8a0a5ba2223c14bc399a714e5d676586cc
Rene Richard <rene.richard@nrc.ca>
2015-05-14 16:15:29 -0300

    Created SimpleVis object

--- src/testwidget/inst/htmlwidgets/testwidget.js
+++ src/testwidget/inst/htmlwidgets/testwidget.js
@@ -5,46 +5,18 @@ HTMLWidgets.widget({
   type: 'output',
 
   initialize: function(el, width, height) {
-      //Create visualization and bind it to element
-      var vis = d3.select(el)
+      //Create div and bind it to element
+      var svg = d3.select(el)
           .append("svg")
           .attr("width", width)
           .attr("height", height);
-
-      return vis;
+      return svg;
   },
 
   renderValue: function(el, x, instance) {
-
       var svg = d3.select(el).select("svg");
-
-      svg.append("rect")
-          .style("stroke", "gray")
-          .style("fill", "white")
-          .attr("x", 10)
-          .attr("y", 10)
-          .attr("width", 50)
-          .attr("height", 50)
-          .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");})
-          .on("mousedown", animateFirstStep);
-
-      function animateFirstStep(){
-          d3.select(this)
-              .transition()
-              .delay(0)
-              .duration(1000)
-              .attr("width", 10)
-              .each("end", animateSecondStep);
-      };
-
-      function animateSecondStep(){
-          d3.select(this)
-              .transition()
-              .duration(1000)
-              .attr("width", 50);
-      };
-
+      var vis = new SimpleVis(svg).draw();
+      return vis;
   },
 
   resize: function(el, width, height, instance) {
